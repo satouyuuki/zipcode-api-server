b014de528df9d0f51dc1194ad8f1f4df47273166
satoyuki <yuki01377@gmail.com>
2021-05-05 02:42:47 +0900

    api 完成

--- main.go
+++ main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
+	"encoding/json"
+	"net/http"
+	"github.com/gorilla/mux"
 	"server2/pkg/api"
 )
 
 func main() {
-	api.FetchIndex()
+	r := mux.NewRouter()
+	r.HandleFunc("/api/", show)
+	// api.FetchIndex()
+	panic(http.ListenAndServe(":8080", r))
+}
+
+func show(w http.ResponseWriter, r *http.Request) {
+	data := api.FetchIndex()
+	// json形式に変換
+	bytes, err := json.Marshal(data)
+	if err != nil {
+		panic(err.Error())
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(string(bytes)))
 }
--- pkg/api/zipcode.go
+++ pkg/api/zipcode.go
@@ -1,30 +1,30 @@
 package api
 
 import (
-	"database/sql"
-	"fmt"
+	// "fmt"
 	"server2/pkg/db"
 )
 
-// type Zipcode struct {
-// 	local_code int
-// 	old_zipcode int
-// 	zipcode int
-// 	pref_kana string
-// 	city_kana string
-// 	town_kana string
-// 	prefectures string
-// 	city string
-// 	town string
-// 	multiple_zipcode int
-// 	koaza int
-// 	tyome int
-// 	multiple_town int
-// 	update_flag int
-// 	update_why int
-// }
+type Zip struct {
+	Id int `json:"id"`
+	LocalCode int `json:"local_code"`
+	OldZipcode int `json:"old_zipcode"`
+	Zipcode int `json:"zipcode"`
+	PrefKana string `json:"pref_kana"`
+	CityKana string `json:"city_kana"`
+	TownKana string `json:"town_kana"`
+	Prefectures string `json:"prefectures"`
+	City string `json:"city"`
+	Town string `json:"town"`
+	MultipleZipcode int `json:"multiple_zipcode"`
+	Koaza int `json:"koaza"`
+	Tyome int `json:"tyome"`
+	MultipleTown int `json:"multiple_town"`
+	UpdateFlag int `json:"update_flag"`
+	UpdateWhy int `json:"update_why"`
+}
 
-func FetchIndex() {
+func FetchIndex() []Zip {
 	db := db.Connect()
 	defer db.Close()
 
@@ -33,47 +33,14 @@ func FetchIndex() {
 		panic(err.Error())
 	}
 
-	columns, err := rows.Columns()
-	if err != nil {
-		panic(err.Error())
-	} 
-
-	values := make([]sql.RawBytes, len(columns))
-
-	// rows.Scanには引数に`[]interface{}`が必要
-
-	scanArgs := make([]interface{}, len(values))
-	// fmt.Println("scanArgs = ", scanArgs)
-	for i := range values {
-		// scanArgs[i]にvalues[i]のポインタを設定
-		scanArgs[i] = &values[i]
-	}
-
-	//Opening型のスライスに格納します
-  // returnValues := make([]Zipcode, len(values))
-
-	// fmt.Println("values = ", values)
-	// 行セットに対して繰り返し処理
+	scanArgs := make([]Zip, 0)
 	for rows.Next() {
-		// columnを変数へ読み込む
-		// fmt.Println("values2 = ", values)
-		// 各行において、row.Scan()でcolumnを変数へ読み込んでいる
-		err = rows.Scan(scanArgs...)
-		// fmt.Println("values3 = ", string(values[3]))
+		var value Zip
+		err = rows.Scan(&value.LocalCode, &value.OldZipcode, &value.Zipcode, &value.PrefKana, &value.CityKana, &value.TownKana, &value.Prefectures, &value.City, &value.Town, &value.MultipleZipcode, &value.Koaza, &value.Tyome, &value.MultipleTown, &value.UpdateFlag, &value.UpdateWhy, &value.Id)
 		if err != nil {
 			panic(err.Error())
 		}
-
-		var value string
-		for i, col := range values {
-			if col == nil {
-				value = "NULL"
-			} else {
-				value = string(col)
-			}
-			fmt.Println(columns[i], ": ", value)
-			// returnValues = append(returnValues, value)
-		}
-		fmt.Println("-------------------------");
+		scanArgs = append(scanArgs, value)
 	}
+	return scanArgs
 }
